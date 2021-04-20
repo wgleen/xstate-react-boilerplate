c3d04a80fa7a6fb9c4057e3508a1ee7c71f5e02f
Wallace Gleen <wallace.gleen@gmail.com>
2021-04-20 12:48:36 -0300

    feat: create routes config

---
+++ src/config/routes.js
@@ -0,0 +1,23 @@
+export default {
+  reviewsIndex: {
+    path: '/reviews'
+  },
+  reviewsCreate: {
+    path: '/reviews/new'
+  },
+  reviewsCreateSelectUniverse: {
+    path: '/reviews/new/universe'
+  },
+  reviewsCreateSelectMovie: {
+    path: '/reviews/new/movie'
+  },
+  reviewsCreateGood: {
+    path: '/reviews/new/good'
+  },
+  reviewsCreateBad: {
+    path: '/reviews/new/bad'
+  },
+  reviewsCreateThanks: {
+    path: '/reviews/new/thanks'
+  }
+}
